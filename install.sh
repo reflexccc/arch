bdeb33fba8d4e7655c3b5be0475e493d02ade681
reflexccc <162376235+reflexccc@users.noreply.github.com>
2024-04-12 15:43:45 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,27 @@
+# install herbstluftwm
+sudo pacman -S herbstluftwm
+mkdir -p ~/.config/herbstluftwm
+cp /etc/xdg/herbstluftwm/autostart ~/.config/herbstluftwm/
+
+# install pikaur
+# sudo pacman -S --needed base-devel git
+# git clone https://aur.archlinux.org/pikaur.git
+# cd pikaur
+# makepkg -fsri
+
+sudo pacman -S kitty
+# inst alacritty
+
+sudo pacman -S nitrogen
+sudo pacman -S galculator
+sudo pacman -S vlc
+
+sudo pacman -S nvim rofi polybar picom chromium
+
+sudo pacman -S btop bat cmatrix flatpak
+
+# flatpaks
+flatpak install flathub com.discordapp.Discord
+flatpak install flathub md.obsidian.Obsidian
+
+cd .config
