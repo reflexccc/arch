0060e66a6425a553a3e5b108435532a1f45bde6c
reflexccc <162376235+reflexccc@users.noreply.github.com>
2024-04-13 08:51:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ sudo pacman -S herbstluftwm
 mkdir -p ~/.config/herbstluftwm
 
 # install pikaur
-# sudo pacman -S --needed base-devel git
-# git clone https://aur.archlinux.org/pikaur.git
-# cd pikaur
-# makepkg -fsri
+sudo pacman -S --needed base-devel git
+git clone https://aur.archlinux.org/pikaur.git
+cd pikaur
+makepkg -fsri
 
 sudo pacman -S kitty
 # inst alacritty
@@ -29,9 +29,25 @@ mv ~/arch/bashrc ~/.bashrc
 mv ~/picom.conf ~/.config/herbstluftwm
 mv ~/autostart ~/.config/herbstluftwm
 
+# rofi
 cd ~/.config/herbstluftwm
 
 git clone --depth=1 https://github.com/adi1090x/rofi.git
 cd rofi
 chmod +x setup.sh
 ./setup.sh
+
+cd ..
+sudo rm -r rofi
+cd ~/.config
+mv -f rofi ~/.config/herbstluftwm
+
+cd rofi/colors
+rm onedark.rasi
+mv ~/arch/onedark.rasi ~/.config/herbstluftwm/rofi/colors
+cd ~/.config/herbstluftwm/rofi/launchers/type-2
+rm launcher.sh /shared/colors.rasi
+mv ~/arch/launcher.sh ~/.config/herbstluftwm/rofi/launchers/type-2 && mv ~/arch/colors.rasi ~/.config/herbstluftwm/rofi/launchers/type-2/shared
+chmod +x ~/.config/herbstluftwm/rofi/launchers/type-2/launcher.sh
+
+
