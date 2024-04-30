fafb27a6e3aa468d22fdd44a67ccb83459e69ecb
reflexccc <162376235+reflexccc@users.noreply.github.com>
2024-04-30 14:36:38 +0200

    Add files via upload

---
+++ config.fish
@@ -0,0 +1,455 @@
+###
+# https://fishshell.com/docs/current/index.html
+# https://github.com/jorgebucaran/cookbook.fish
+
+# themes
+# https://github.com/oh-my-fish/oh-my-fish/blob/master/docs/Themes.md
+
+# Plugins
+# https://github.com/jethrokuan/fzf
+# https://github.com/IlanCosman/tide.git - fisher install IlanCosman/tide@v5
+# https://github.com/jhillyerd/plugin-git
+
+# tools
+# https://github.com/jorgebucaran/fisher
+# https://github.com/oh-my-fish/oh-my-fish
+# https://github.com/danhper/fundle
+
+#set VIRTUAL_ENV_DISABLE_PROMPT "1"
+
+if not status --is-interactive
+  exit
+end
+
+# Load private config
+if [ -f $HOME/.config/fish/private.fish ]
+    source $HOME/.config/fish/private.fish
+end
+
+# Git
+if [ -f $HOME/.config/fish/git.fish ]
+    source $HOME/.config/fish/git.fish
+end
+
+# Aliases
+if [ -f $HOME/.config/fish/alias.fish ]
+    source $HOME/.config/fish/alias.fish
+end
+
+# reload fish config
+function reload
+    exec fish
+    set -l config (status -f)
+    echo "reloading: $config"
+end
+
+# User paths
+set -e fish_user_paths
+set -U fish_user_paths $HOME/.bin $HOME/.local/bin $HOME/Applications $fish_user_paths
+
+# Starship prompt
+#if command -sq starship
+#    starship init fish | source
+#end
+
+# sets tools
+set -x EDITOR nvim
+set -x VISUAL nvim
+#set -x TERM alacritty
+# Sets the terminal type for proper colors
+set TERM "xterm-256color"
+
+# Suppresses fish's intro message
+set fish_greeting
+#function fish_greeting
+#    fish_logo
+#end
+
+# Prevent directories names from being shortened
+set fish_prompt_pwd_dir_length 0
+set -x FZF_DEFAULT_OPTS "--color=16,header:13,info:5,pointer:3,marker:9,spinner:1,prompt:5,fg:7,hl:14,fg+:3,hl+:9 --inline-info --tiebreak=end,length --bind=shift-tab:toggle-down,tab:toggle-up"
+# "bat" as manpager
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+set -x MANROFFOPT "-c"
+set -g theme_nerd_fonts yes
+
+
+if status --is-login
+    set -gx PATH $PATH ~/.bin
+end
+
+if status --is-login
+    set -gx PATH $PATH ~/.local/bin
+end
+
+if type -q bat
+    alias cat="bat --paging=never"
+end
+
+if command -sq fzf && type -q fzf_configure_bindings
+  fzf_configure_bindings --directory=\ct
+end
+
+if not set -q -g fish_user_abbreviations
+  set -gx fish_user_abbreviations
+end
+
+#if type -f fortune >/dev/null
+#  set -l fortune "fortune -a"
+#  if type -f lolcat >/dev/null
+#    set fortune "$fortune | lolcat"
+#  end
+#  eval $fortune
+#  echo
+#end
+
+if test tree >/dev/null
+    function l1;  tree --dirsfirst -ChFL 1 $argv; end
+    function l2;  tree --dirsfirst -ChFL 2 $argv; end
+    function l3;  tree --dirsfirst -ChFL 3 $argv; end
+    function ll1; tree --dirsfirst -ChFupDaL 1 $argv; end
+    function ll2; tree --dirsfirst -ChFupDaL 2 $argv; end
+    function ll3; tree --dirsfirst -ChFupDaL 3 $argv; end
+end
+
+if type -q direnv
+    eval (direnv hook fish)
+end
+
+
+
+### FUNCTIONS ###
+# Fish command history
+function history
+    builtin history --show-time='%F %T ' | sort
+end
+
+# Make a backup file
+function backup --argument filename
+    cp $filename $filename.bak
+end
+
+# recently installed packages
+function ripp --argument length -d "List the last n (100) packages installed"
+    if test -z $length
+        set length 100
+    end
+    expac --timefmt='%Y-%m-%d %T' '%l\t%n' | sort | tail -n $length | nl
+end
+
+function gl
+    git log --graph --color=always --format="%C(auto)%h%d %s %C(black)%C(bold)%cr" $argv | fzf --ansi --no-sort --reverse --tiebreak=index --toggle-sort=\` --bind "ctrl-m:execute: echo '{}' | grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always % | less -R'"
+end
+
+function ex --description "Extract bundled & compressed files"
+    if test -f "$argv[1]"
+        switch $argv[1]
+            case '*.tar.bz2'
+                tar xjf $argv[1]
+            case '*.tar.gz'
+                tar xzf $argv[1]
+            case '*.bz2'
+                bunzip2 $argv[1]
+            case '*.rar'
+                unrar $argv[1]
+            case '*.gz'
+                gunzip $argv[1]
+            case '*.tar'
+                tar xf $argv[1]
+            case '*.tbz2'
+                tar xjf $argv[1]
+            case '*.tgz'
+                tar xzf $argv[1]
+            case '*.zip'
+                unzip $argv[1]
+            case '*.Z'
+                uncompress $argv[1]
+            case '*.7z'
+                7z $argv[1]
+            case '*.deb'
+                ar $argv[1]
+            case '*.tar.xz'
+                tar xf $argv[1]
+            case '*.tar.zst'
+                tar xf $argv[1]
+            case '*'
+                echo "'$argv[1]' cannot be extracted via ex"
+        end
+   else
+       echo "'$argv[1]' is not a valid file"
+   end
+end
+
+function less
+    command less -R $argv
+end
+
+function cd
+    builtin cd $argv; and ls
+end
+
+### ALIASES ###
+
+#list
+alias ls="ls --color=auto"
+alias la="ls -a --color=auto"
+alias listdir="ls -d */ > list"
+
+#pacman
+alias pacs='sudo pacman -S'
+alias pacr='sudo pacman -R'
+alias pacrs='sudo pacman -Rs'
+alias pacup='sudo pacman -Syu'
+
+# show the list of packages that need this package - depends mpv as example
+function function_depends
+    set search $argv[1]
+    sudo pacman -Sii $search | grep "Required" | sed -e "s/Required By     : //g" | sed -e "s/  /\n/g"
+end
+
+alias depends='function_depends'
+
+if type -q exa
+    alias ls="exa"
+    alias xls="exa -a --icons --color=always --group-directories-first"
+    alias xll="exa -lag --icons --color=always --group-directories-first --octal-permissions"
+end
+
+#fix obvious typo's
+alias ..="cd .."
+alias update="sudo pacman -Syyu"
+
+## Colorize the grep command output for ease of use (good for log files)##
+alias grep="grep --color=auto"
+alias egrep="egrep --color=auto"
+alias fgrep="fgrep --color=auto"
+
+# Color output of ip
+alias ip="ip -color"
+
+#readable output
+alias df="df -h"
+
+#keyboard
+alias qwertz="sudo localectl set-x11-keymap de"
+
+#which graphical card is working
+alias whichvga="/usr/local/bin/arcolinux-which-vga"
+
+#free
+alias free="free -mt"
+
+#continue download
+alias wget="wget -c"
+
+#userlist
+alias userlist="cut -d: -f1 /etc/passwd | sort"
+
+#merge new settings
+alias merge="xrdb -merge ~/.Xresources"
+
+# Aliases for software managment
+# pacman
+alias pacman="sudo pacman --color auto"
+
+# paru as aur helper - updates everything
+alias pikr="pikaur"
+alias pikup="pikaur -Syu"
+
+#ps
+alias psa="ps auxf"
+alias psgrep="ps aux | grep -v grep | grep -i -e VSZ -e"
+
+#grub update
+alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+alias grub-update="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+#grub issue 08/2022
+alias install-grub-efi="sudo grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArcoLinux"
+
+#copy/paste all content of /etc/skel over to home folder - backup of config created - beware
+#skel alias has been replaced with a script at /usr/local/bin/skel
+
+#backup contents of /etc/skel to hidden backup folder in home/user
+alias bupskel="cp -Rf /etc/skel ~/.skel-backup-(date +%Y.%m.%d-%H.%M.%S)"
+
+#switch between bash, zsh and fish
+alias tobash="sudo chsh $USER -s /bin/bash && echo 'Done. Now log out.'"
+alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Done. Now log out.'"
+alias tofish="sudo chsh $USER -s /bin/fish && echo 'Done. Now log out.'"
+
+#switch between displaymanager or bootsystem
+alias tosddm="sudo pacman -S sddm --noconfirm --needed ; sudo systemctl enable sddm.service -f ; echo 'Sddm is active - reboot now'"
+
+# kill commands
+# quickly kill polybar
+alias kp="killall waybar"
+
+#hardware info --short
+alias hw="hwinfo --short"
+
+#audio check pulseaudio or pipewire
+alias audio="pactl info | grep 'Server Name'"
+
+#approximation of how old your hardware is
+alias howold="sudo lshw | grep -B 3 -A 8 BIOS"
+
+#check cpu
+alias cpu="cpuid -i | grep uarch | head -n 1"
+
+#get fastest mirrors in your neighborhood
+alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
+alias mirrord="sudo reflector --latest 30 --number 10 --sort delay --save /etc/pacman.d/mirrorlist"
+alias mirrors="sudo reflector --latest 30 --number 10 --sort score --save /etc/pacman.d/mirrorlist"
+alias mirrora="sudo reflector --latest 30 --number 10 --sort age --save /etc/pacman.d/mirrorlist"
+#our experimental - best option for the moment
+alias mirrorx="sudo reflector --age 6 --latest 20  --fastest 20 --threads 5 --sort rate --protocol https --save /etc/pacman.d/mirrorlist"
+alias mirrorxx="sudo reflector --age 6 --latest 20  --fastest 20 --threads 20 --sort rate --protocol https --save /etc/pacman.d/mirrorlist"
+alias ram="rate-mirrors --allow-root --disable-comments arch | sudo tee /etc/pacman.d/mirrorlist"
+alias rams="rate-mirrors --allow-root --disable-comments --protocol https arch  | sudo tee /etc/pacman.d/mirrorlist"
+
+#mounting the folder Public for exchange between host and guest on virtualbox
+alias vbm="sudo /usr/local/bin/arcolinux-vbox-share"
+
+#Recent Installed Packages
+alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
+alias riplong="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -3000 | nl"
+
+#iso and version used to install ArcoLinux
+alias iso="cat /etc/dev-rel | awk -F '=' '/ISO/ {print $2}'"
+alias isoo="cat /etc/dev-rel"
+
+#Cleanup orphaned packages
+alias cleanup="sudo pacman -Rns (pacman -Qtdq)"
+
+# This will generate a list of explicitly installed packages
+alias list="sudo pacman -Qqe"
+#This will generate a list of explicitly installed packages without dependencies
+alias listt="sudo pacman -Qqet"
+# list of AUR packages
+alias listaur="sudo pacman -Qqem"
+# add > list at the end to write to a file
+
+# install packages from list
+# pacman -S --needed - < my-list-of-packages.txt
+
+#clear
+alias clean="clear; seq 1 (tput cols) | sort -R | sparklines | lolcat"
+
+#search content with ripgrep
+alias rg="rg --sort path"
+
+#get the error messages from journalctl
+alias jctl="journalctl -p 3 -xb"
+
+#nano for important configuration files
+#know what you do in these files
+alias nf="$EDITOR ~/.config/fish/config.fish"
+alias nneofetch="$EDITOR ~/.config/neofetch/config.conf"
+
+#reading logs with bat
+alias lcalamares="bat /var/log/Calamares.log"
+alias lpacman="bat /var/log/pacman.log"
+alias lxorg="bat /var/log/Xorg.0.log"
+alias lxorgo="bat /var/log/Xorg.0.log.old"
+
+#systeminfo
+alias probe="sudo -E hw-probe -all -upload"
+alias sysfailed="systemctl list-units --failed"
+
+#shutdown or reboot
+alias ssn="sudo shutdown now"
+alias sr="reboot"
+
+#update betterlockscreen images
+alias bls="betterlockscreen -u /usr/share/backgrounds/arcolinux/"
+
+#give the list of all installed desktops - xsessions desktops
+alias xd="ls /usr/share/xsessions"
+alias xdw="ls /usr/share/wayland-sessions"
+
+#wayland aliases
+alias wsimplescreen="wf-recorder -a"
+alias wsimplescreenrecorder="wf-recorder -a -c h264_vaapi -C aac -d /dev/dri/renderD128 --file=recording.mp4"
+
+#give a list of the kernels installed
+alias kernel="ls /usr/lib/modules"
+alias kernels="ls /usr/lib/modules"
+
+#am I on grub,systemd-boot or refind
+alias boot="sudo /usr/local/bin/arcolinux-boot"
+
+#btrfs aliases
+alias btrfsfs="sudo btrfs filesystem df /"
+alias btrfsli="sudo btrfs su li / -t"
+
+#git
+alias gc="git clone"
+
+#meine aliase
+alias v="nvim"
+alias sv="sudo nvim"
+alias clr="clear"
+
+#moving your personal files and folders from /personal to ~
+function personal
+    cp -rf /personal/ ~
+    cp -rf /personal/.* ~
+end
+
+# git
+# using plugin
+# omf install https://github.com/jhillyerd/plugin-git
+alias undopush "git push -f origin HEAD^:master"
+
+# reporting tools - install when not installed
+neofetch | lolcat
+#screenfetch
+#alsi
+#paleofetch
+#fetch
+#hfetch
+#sfetch
+#ufetch
+#ufetch-arco
+#pfetch
+#sysinfo
+#sysinfo-retro
+#cpufetch
+#colorscript random
+#hyfetch
+
+#cowsay -f stegosaurus "Hello!!!"| lolcat
+
+# colors to set or unset
+
+set fish_color_autosuggestion "#969896"
+set fish_color_cancel -r
+set fish_color_command "#0782DE"
+set fish_color_comment "#f0c674"
+set fish_color_cwd "#008000"
+set fish_color_cwd_root red
+set fish_color_end "#b294bb"
+set fish_color_error "#fb4934"
+set fish_color_escape "#fe8019"
+set fish_color_history_current --bold
+set fish_color_host "#85AD82"
+set fish_color_host_remote yellow
+set fish_color_match --background=brblue
+set fish_color_normal normal
+set fish_color_operator "#fe8019"
+set fish_color_param "#81a2be"
+set fish_color_quote "#b8bb26"
+set fish_color_redirection "#d3869b"
+set fish_color_search_match bryellow background=brblack
+set fish_color_selection white --bold background=brblack
+set fish_color_status red
+set fish_color_user brgreen
+set fish_color_valid_path --underline
+set fish_pager_color_completion normal
+set fish_pager_color_description "#B3A06D" yellow
+set fish_pager_color_prefix normal --bold underline
+set fish_pager_color_prefix white --bold --underline
+set fish_pager_color_progress brwhite --background=cyan
+set fish_color_search_match --background="#60AEFF"
+
+starship init fish | source
