3853d6f3172b21612a423dc790632f27ba857ab0
reflexccc <162376235+reflexccc@users.noreply.github.com>
2024-06-21 17:12:15 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,11 @@
-sudo pacman -S kitty
-# inst alacritty
+sudo pacman -S alacritty galculator vlc neovim neofetch btop bat cmatrix flatpak obsidian zsh zsh-autocomplete pamixer ranger
 
-sudo pacman -S nitrogen galculator vlc
+# git clone --depth=1 https://github.com/adi1090x/rofi.git
+# cd rofi
+# chmod +x setup.sh
+# ./setup.sh
 
-sudo pacman -S neovim rofi polybar picom chromium galculator neofetch btop bat cmatrix flatpak
-
-# flatpaks
-flatpak install flathub com.discordapp.Discord md.obsidian.Obsidian org.onlyoffice.desktopeditors com.github.IsmaelMartinez.teams_for_linux
-
-mv ~/arch/bashrc ~/.bashrc
-mv ~/picom.conf ~/.config/herbstluftwm
-mv ~/autostart ~/.config/herbstluftwm
-
-# rofi
-cd ~/.config/herbstluftwm
-
-git clone --depth=1 https://github.com/adi1090x/rofi.git
-cd rofi
-chmod +x setup.sh
-./setup.sh
-
-cd ..
-sudo rm -r rofi
-cd ~/.config
-mv -f rofi ~/.config/herbstluftwm
-
-cd rofi/colors
-rm onedark.rasi
-mv ~/arch/onedark.rasi ~/.config/herbstluftwm/rofi/colors
-cd ~/.config/herbstluftwm/rofi/launchers/type-2
-rm launcher.sh /shared/colors.rasi
-mv ~/arch/launcher.sh ~/.config/herbstluftwm/rofi/launchers/type-2 && mv ~/arch/colors.rasi ~/.config/herbstluftwm/rofi/launchers/type-2/shared
-chmod +x ~/.config/herbstluftwm/rofi/launchers/type-2/launcher.sh
-
-
+mv ~/Downloads/arch/hyprland.conf ~/.config/hypr/hyprland.conf
+mv ~/Downloads/arch/style.css ~/.config/waybar/style.css
+mv ~/Downloads/arch/config.fish ~/.config/fish/config.fish
+mv ~/Downloads/arch/config ~/.config/waybar/config
