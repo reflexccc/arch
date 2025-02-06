3b4a0dddd75bd20cf2804f22d984a7dbdff5e2f8
reflexccc <162376235+reflexccc@users.noreply.github.com>
2025-02-06 16:21:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,9 @@
-sudo pacman -S alacritty galculator vlc neovim neofetch btop bat cmatrix flatpak zsh zsh-autocomplete pamixer ranger
+sudo pacman -S alacritty galculator vlc neovim neofetch btop bat flatpak zsh zsh-autocomplete pamixer ranger teams-for-linux firefox auto-cpufreq
 
-# git clone --depth=1 https://github.com/adi1090x/rofi.git
-# cd rofi
-# chmod +x setup.sh
-# ./setup.sh
+git clone https://[token]@github.com/reflexccc/Obsidian_Auto.git
 
 mv ~/Downloads/arch/hyprland.conf ~/.config/hypr/hyprland.conf
 mv ~/Downloads/arch/style.css ~/.config/waybar/style.css
 mv ~/Downloads/arch/config.fish ~/.config/fish/config.fish
 mv ~/Downloads/arch/config ~/.config/waybar/config
+
