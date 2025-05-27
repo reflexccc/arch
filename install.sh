8ce682e64710347d358f020f291bba8782aa51c8
reflexccc <162376235+reflexccc@users.noreply.github.com>
2025-05-27 15:05:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
-sudo pacman -S alacritty galculator vlc neovim neofetch btop bat flatpak zsh zsh-autocomplete pamixer ranger teams-for-linux auto-cpufreq betterbird-de-bin
+sudo pacman -S alacritty galculator vlc vim yay neovim neofetch btop bat zsh zsh-autocomplete pamixer teams-for-linux auto-cpufreq betterbird-bin obsidian wofi vscodium dolphin lolcat cowsay flameshot spotify
+
+cd ~/Downloads
 
 git clone https://[token]@github.com/reflexccc/Obsidian_Auto.git
+git clone https://[token]@github.com/reflexccc/arch.git 
 
-mv ~/Downloads/arch/hyprland.conf ~/.config/hypr/hyprland.conf
-mv ~/Downloads/arch/style.css ~/.config/waybar/style.css
+mv ~/Downloads/arch/hyprland_config ~/.config/hypr/hyprland.conf
+mv ~/Downloads/arch/waybar_style ~/.config/waybar/style.css
 mv ~/Downloads/arch/config.fish ~/.config/fish/config.fish
 mv ~/Downloads/arch/config ~/.config/waybar/config
 
